0c82da2068a4f1ac9bf4d422c618eedb40d7aaf8
craigm <craiglmccarthy@gmail.com>
2022-02-20 22:46:24 +0000

    Initial commit

---
+++ domain-starter/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import App from './App';
+import './fonts/matisse/FOT-MatisseV Pro EB.otf';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
